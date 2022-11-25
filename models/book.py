61f7ad2aae9e83c49f29d11a885ad9ef0ce30a6f
yudur <duarteyudi218@gmail.com>
2022-11-24 21:09:41 -0300

    Flask_sqlalchemy

---
+++ models/book.py
@@ -0,0 +1,38 @@
+from db import db
+
+class BookModel(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False, unique=True)
+    pages = db.Column(db.Float(precision=2), nullable=False)
+
+    def __init__(self, title, pages):
+        self.title = title
+        self.pages = pages
+
+    def __repr__(self) -> str:
+        return f'BookModel(title={self.title},  pages={self.pages})'
+
+    def json(self):
+        return {'title': self.title, 'pages': self.pages}
+
+    @classmethod
+    def find_by_title(cls, title):
+        return cls.query.filter_by(title=title).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
